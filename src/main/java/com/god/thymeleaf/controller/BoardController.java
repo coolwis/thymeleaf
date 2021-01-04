ac654b9883e2dd6754bd8760f90395b28e80bf77
New Trend developer <wisdombrave@gmail.com>
2021-01-04 17:39:06 +0900

    lay out 구성

---
+++ src/main/java/com/god/thymeleaf/controller/BoardController.java
@@ -0,0 +1,18 @@
+package com.god.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @GetMapping("/list")
+    public String list(){
+        return  "board/list";
+    }
+
+}
+
